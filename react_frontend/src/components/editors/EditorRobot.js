85d3dcc584ae8a41c13af5d4ba803c9b60c3cb13
Javier Izquierdo Hernandez <javizqh@gmail.com>
2024-10-28 20:47:32 +0100

    Debugging

--- react_frontend/src/components/editors/EditorRobot.js
+++ react_frontend/src/components/editors/EditorRobot.js
@@ -58,6 +58,7 @@ export default function EditorRobot(props) {
       detail: { isLoading: false },
     });
     window.dispatchEvent(codeLoadedEvent);
+    console.log("event")
   }, []);
 
   //! Monaco Code Editor
