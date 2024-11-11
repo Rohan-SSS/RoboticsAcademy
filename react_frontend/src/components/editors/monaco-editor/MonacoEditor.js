d502d1cbba7fd9a93cc0700a523f6d26b91e3d23
Javier Izquierdo Hernandez <javizqh@gmail.com>
2024-11-11 18:29:36 +0100

    More debug

--- react_frontend/src/components/editors/monaco-editor/MonacoEditor.js
+++ react_frontend/src/components/editors/monaco-editor/MonacoEditor.js
@@ -28,6 +28,7 @@ const MonacoEditor = ({
   const lineNumberDecorationRef = useRef(null);
   // Rducer state
   const { isLoading, monacoEditorTheme, editorOptions, baseUrl } = state;
+  console.log(baseUrl)
   // USE STATE
   const [lineNumber, setLineNumber] = useState(-1);
   const [lineNumberDecorations, setLineNumberDecorations] = useState([]);
