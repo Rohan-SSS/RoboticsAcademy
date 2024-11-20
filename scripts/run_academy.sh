54c7f4fca512f640c6ec0c18839a89ffad2542d5
Javier Izquierdo <124056014+javizqh@users.noreply.github.com>
2024-11-20 22:32:23 +0100

    Update run_academy.sh

--- scripts/run_academy.sh
+++ scripts/run_academy.sh
@@ -5,7 +5,7 @@ gpu_mode="false"
 nvidia="false"
 base_path_offline="compose_cfg/"
 compose_file="user_humble_cpu"
-
+base_path_online="https://raw.githubusercontent.com/JdeRobot/RoboticsAcademy/humble-devel/compose_cfg/"
 
 # Function to clean up the containers
 cleanup() {
