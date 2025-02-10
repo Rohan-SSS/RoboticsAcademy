162914c3df4db25b097c18a23bee4a30c9fdfd7d
Javier Izquierdo Hernandez <javizqh@gmail.com>
2025-02-10 13:14:34 +0100

    Return to async

--- react_frontend/src/components/visualizers/WorldSelector.js
+++ react_frontend/src/components/visualizers/WorldSelector.js
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
 import MenuItem from "@mui/material/MenuItem";
 import { FormControl, InputLabel, Select, Box } from "@mui/material";
+
 export default function WorldSelector(props) {
   const exerciseConfig = JSON.parse(
     document.getElementById("exercise-config").textContent
   );
   const [disabled, setDisabled] = useState(true);
-  const [selectedCircuit, setSelectedCircuit] = useState(exerciseConfig[0]);
+  const [selectedUniverse, setSelectedUniverse] = useState(exerciseConfig[0]);
   const [configurations, setConfigurations] = useState(exerciseConfig);
 
   useEffect(() => {
     context.mapSelected = exerciseConfig[0].name;
+
     const callback = (message) => {
       if (message.data.state !== "connected") {
         setDisabled(false);
@@ -18,10 +20,12 @@ export default function WorldSelector(props) {
         setDisabled(true);
       }
     };
+
     window.RoboticsExerciseComponents.commsManager.subscribe(
       [window.RoboticsExerciseComponents.commsManager.events.STATE_CHANGED],
       callback
     );
+
     return () => {
       window.RoboticsExerciseComponents.commsManager.unsubscribe(
         [window.RoboticsExerciseComponents.commsManager.events.STATE_CHANGED],
@@ -30,41 +34,29 @@ export default function WorldSelector(props) {
     };
   }, []);
 
-  const handleCircuitChange = (config) => {
+  const handleUniverse = async (config) => {
     context.mapSelected = config.name;
-    setSelectedCircuit(config);
-    console.log(config.visualization);
-    window.RoboticsExerciseComponents.commsManager
-      .terminate_application()
-      .then(() => {
-        window.RoboticsExerciseComponents.commsManager
-          .terminate_visualization()
-          .then(() => {
-            window.RoboticsExerciseComponents.commsManager
-              .terminate_universe()
-              .then(() => {
-                window.RoboticsReactComponents.MessageSystem.Loading.showLoading(
-                  "Launching Universe"
-                );
-                window.RoboticsExerciseComponents.commsManager
-                  .launchWorld({
-                    world: config[0].world,
-                    robot: config[0].robot,
-                  })
-                  .then(() => {
-                    window.RoboticsExerciseComponents.commsManager
-                      .prepareVisualization(config.visualization)
-                      .then(() => {
-                        RoboticsReactComponents.MessageSystem.Loading.hideLoading();
-                        RoboticsReactComponents.MessageSystem.Alert.showAlert(
-                          "Exercise loaded successfully.",
-                          "success"
-                        );
-                      });
-                  });
-              });
-          });
-      });
+    setSelectedUniverse(config);
+    console.log(config);
+
+    await window.RoboticsExerciseComponents.commsManager.terminate_application();
+    await window.RoboticsExerciseComponents.commsManager.terminate_visualization();
+    await window.RoboticsExerciseComponents.commsManager.terminate_universe();
+    window.RoboticsReactComponents.MessageSystem.Loading.showLoading(
+      "Launching Universe"
+    );
+    await window.RoboticsExerciseComponents.commsManager.launchWorld({
+      world: config.world,
+      robot: config.robot,
+    });
+    await window.RoboticsExerciseComponents.commsManager.prepareVisualization(
+      config.world.visualization
+    );
+    RoboticsReactComponents.MessageSystem.Loading.hideLoading();
+    RoboticsReactComponents.MessageSystem.Alert.showAlert(
+      "Exercise loaded successfully.",
+      "success"
+    );
   };
 
   return exerciseConfig.length > 0 ? (
@@ -79,15 +71,15 @@ export default function WorldSelector(props) {
         }}
         size="small"
       >
-        <InputLabel id={"circuit-selector-label"}>Universe</InputLabel>
+        <InputLabel id={"universe-selector-label"}>Universe</InputLabel>
         <Select
           disabled={disabled}
-          value={selectedCircuit}
-          labelId="circuit-selector-label"
-          id={"circuit-selector"}
-          label={"Circuit"}
+          value={selectedUniverse}
+          labelId="universe-selector-label"
+          id={"universe-selector"}
+          label={"Universe"}
           onChange={(e) => {
-            handleCircuitChange(e.target.value);
+            handleUniverse(e.target.value);
           }}
         >
           {configurations.map((option) => (
